c5821535ce0c5c0261ee917d1ab65508d9c5cec6
akshathamn <akshathamn05@gmail.com>
2019-07-26 16:40:01 +0530

    commited

--- src/Controllers/Addproductcontroller.js
+++ src/Controllers/Addproductcontroller.js
@@ -12,6 +12,19 @@ res.json(data);
 
 
 
+// const express = require('express');
+// const router = express.Router();
+
+// const Products = require('../models/Product');
+
+// router.get('/', async (req, res) => {
+//   const products = await Products.find({});
+//   res.json({ products });
+// });
+
+// module.exports = router;
+
+
 // const Validator = require("validator");
 // const isEmpty = require("./is-empty");
 
